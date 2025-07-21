6570e83f5137cf883c118bfe0eb683adc8d4fd62
Giovanni Saucedo <giovamni.sau@gmail.com>
2025-07-20 20:53:08 -0600

    chore: Act3

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Esto ayuda a que la compilación pase a pesar de los warnings/errores de ESLint
   },
-  output: 'standalone',
-  // Añade esta configuración para Prisma
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
-  },
+  // --- AÑADE ESTA LÍNEA CLAVE PARA OPTIMIZAR EL DESPLIEGUE EN SERVERLESS ---
+  output: 'standalone', 
 };
 
 module.exports = nextConfig;
